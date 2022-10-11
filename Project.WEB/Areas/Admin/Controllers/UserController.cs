3ed39f4c79a18e0e697e0e7e8faeaca2278aa278
Fatih Şallı <mimsallifatih@gmail.com>
2022-10-11 10:13:03 +0300

    UserController

---
+++ Project.WEB/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project.WEB.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public UserController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var nesne = userManager.Users;
+            return View(userManager.Users);
+        }
+    }
+}
